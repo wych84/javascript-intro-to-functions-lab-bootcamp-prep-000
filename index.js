b8d30df2a2199121c2130f8636ad5c1c58435e68
wooyangee@gmail.com <wooyangee@gmail.com>
2018-03-31 06:28:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ return "I can't hear you!"
 
 } else if (string === string.toUpperCase()){
 return "YES INDEED!"
-   } else if (string === "I love you, Grandma.")
+   } else if (string === "I love you, Grandma."){
    return "I love you, too."
 +  }
