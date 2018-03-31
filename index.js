9ee75ee54e56824de136adf36977c69cb14ad1fd
wooyangee@gmail.com <wooyangee@gmail.com>
2018-03-31 06:19:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,11 +11,4 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  if(string.toUpperCase() === string) {
-    return "I can't hear you!"
-  }
-} else if (string.toLowerCase() === string) {
-  return "YES INDEED!"
-} else if (string === "I love you, Grandma.") {
-  return "I love you, too."
-}
+ 
